b8f07527bb9bb8e45cb1afecc7a12803d9fdfd8b
陶发齐 <faqi.tao@gewara.com>
2016-09-05 17:04:02 +0800

    增加了格瓦拉的下载

--- app/src/main/java/com/aspsine/multithreaddownload/demo/DataSource.java
+++ app/src/main/java/com/aspsine/multithreaddownload/demo/DataSource.java
@@ -14,6 +14,9 @@ public class DataSource {
     private static DataSource sDataSource = new DataSource();
 
     private static final String[] NAMES = {
+            "格瓦拉-应用宝",
+            "格瓦拉-FIR",
+
             "网易云音乐",
             "优酷",
             "腾讯视频",
@@ -28,6 +31,9 @@ public class DataSource {
     };
 
     private static final String[] IMAGES = {
+            "http://pp.myapp.com/ma_icon/0/icon_5034_1471514862/96",
+            "http://firicon.fir.im/09908021dc7454364a4266ca1334145acd5a0bcd",
+
             "http://img.wdjimg.com/mms/icon/v1/d/f1/1c8ebc9ca51390cf67d1c3c3d3298f1d_512_512.png",
             "http://img.wdjimg.com/mms/icon/v1/3/2d/dc14dd1e40b8e561eae91584432262d3_512_512.png",
             "http://img.wdjimg.com/mms/icon/v1/8/10/1b26d9f0a258255b0431c03a21c0d108_512_512.png",
@@ -42,6 +48,9 @@ public class DataSource {
     };
 
     private static final String[] URLS = {
+            "http://imtt.dd.qq.com/16891/2ED1B174F59B26D50A7801F4F3332254.apk?fsname=com.gewara_7.0.1_69.apk&csr=4d5s",
+            "http://pkg3.fir.im/a2a3389b2f2bd75ca1c3e9cb318892fbb6c24030.apk?attname=gewara_v7.1.0_2016-09-05_AS04.apk_7.1.0.apk",
+
             "http://s1.music.126.net/download/android/CloudMusic_2.8.1_official_4.apk",
             "http://dl.m.cc.youku.com/android/phone/Youku_Phone_youkuweb.apk",
             "http://dldir1.qq.com/qqmi/TencentVideo_V4.1.0.8897_51.apk",
