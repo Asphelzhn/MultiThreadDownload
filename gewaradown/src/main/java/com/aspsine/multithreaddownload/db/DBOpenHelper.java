43756b6de6ce0ebea5d3a28661ada70c83e1b996
陶发齐 <faqi.tao@gewara.com>
2016-09-06 10:49:32 +0800

    change code

--- gewaradown/src/main/java/com/aspsine/multithreaddownload/db/DBOpenHelper.java
+++ gewaradown/src/main/java/com/aspsine/multithreaddownload/db/DBOpenHelper.java
@@ -6,18 +6,18 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DBOpenHelper extends SQLiteOpenHelper {
 
-    private static final String DB_NAME = "download.db";
+    private static final String DB_NAME = "gewara.db";
     private static final int DB_VERSION = 1;
 
     public DBOpenHelper(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
     }
 
-    void createTable(SQLiteDatabase db){
+    void createTable(SQLiteDatabase db) {
         ThreadInfoDao.createTable(db);
     }
 
-    void dropTable(SQLiteDatabase db){
+    void dropTable(SQLiteDatabase db) {
         ThreadInfoDao.dropTable(db);
     }
 
